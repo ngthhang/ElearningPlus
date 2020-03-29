542d8d55c5ba16d7ad0b8c8309c851ee9660f51d
sieutini4444 <sieutini4444@gmail.com>
2020-03-29 20:29:10 +0700

    test git

--- app/src/main/java/com/example/elearningplus/ProfileScreenActivity.java
+++ app/src/main/java/com/example/elearningplus/ProfileScreenActivity.java
@@ -10,5 +10,7 @@ public class ProfileScreenActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_profile_screen);
+
     }
+    //test git
 }
