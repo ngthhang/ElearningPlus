5bdc9b3bbc5b433f40327096aebe0bff3415cdab
sieutini4444 <sieutini4444@gmail.com>
2020-04-02 19:11:58 +0700

    Full giao diện (profile)

--- app/src/main/java/com/example/elearningplus/ProfileScreenActivity.java
+++ app/src/main/java/com/example/elearningplus/ProfileScreenActivity.java
@@ -9,6 +9,9 @@ import android.graphics.BitmapFactory;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageButton;
 import android.widget.ImageView;
 import android.widget.ListView;
 
@@ -21,18 +24,20 @@ import java.util.List;
 public class ProfileScreenActivity extends AppCompatActivity {
     ListView listView;
     List<Profile_DiemSV> mlist;
+    ImageButton imageButton;
+    Button button;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_profile_screen);
-        /*
-        Bitmap bitmap = BitmapFactory.decodeResource(this.getResources(), R.mipmap.giang);
+
+        //Circling Avatar
+        Bitmap bitmap = BitmapFactory.decodeResource(this.getResources(), R.drawable.giang);
         Bitmap circularBitmap = Profile_ImageConverter.getRoundedCornerBitmap(bitmap, 100);
 
-        ImageView circularImageView = (ImageView) findViewById(R.id.imageView2);
+        ImageView circularImageView = (ImageView) findViewById(R.id.imgAvatar);
         circularImageView.setImageBitmap(circularBitmap);
-*/
 
         /*START - HANDLE BOTTOM NAVIGATION */
         //Initial and assign variable
@@ -61,15 +66,42 @@ public class ProfileScreenActivity extends AppCompatActivity {
             }
         });
         /*FINISH - HANDLE BOTTOM NAVIGATION*/
+
+        //Add database DiemSV
         mlist = new ArrayList<>();
-        int i=0;
-        for (i=0;i<=10;i++) {
-            mlist.add(new Profile_DiemSV("Cấu Trúc Rời Rạc", (float) 8.5));
-        }
+
+        mlist.add(new Profile_DiemSV("CTRR",(float) 7.5));
+        mlist.add(new Profile_DiemSV("MMT",(float) 8));
+        mlist.add(new Profile_DiemSV("NMHĐH",(float) 6));
+        mlist.add(new Profile_DiemSV("SXTK",(float) 8.2));
+        mlist.add(new Profile_DiemSV("PTƯDDĐ",(float) 9.3));
+
+        //Truyền dữ liệu vào adapter
         listView=findViewById(R.id.listDiemSV);
         Profile_DiemSV_Adapter adapter=new Profile_DiemSV_Adapter(this,mlist);
         listView.setAdapter(adapter);
         adapter.notifyDataSetChanged();
+
+        //Thiết lập sự kiện button log out
+        button = findViewById(R.id.btnLogOut);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(ProfileScreenActivity.this,LoginScreenActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        //Thiết lập sự kiện imagebutton Camera
+        imageButton = findViewById(R.id.imgbCamera);
+        imageButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //
+            }
+        });
+
+
     }
 
     @Override
