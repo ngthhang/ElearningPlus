848aff953435494893ad7749bdf29b9f7358ae69
yumi298 <yuminguyen2982000@gmail.com>
2020-04-02 19:56:19 +0700

    Login

--- app/src/main/java/com/example/elearningplus/LoginScreenActivity.java
+++ app/src/main/java/com/example/elearningplus/LoginScreenActivity.java
@@ -7,27 +7,33 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
+import android.widget.Toast;
 
 public class LoginScreenActivity extends AppCompatActivity {
-    Button btnClick;
-    TextView txtNd;
+    Button btnLogin,btnForgot;
+    TextView txtUser,txtPass;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login_screen);
-    btnClick=(Button) findViewById(R.id.loginBut);
-
-    btnClick.setOnClickListener(new View.OnClickListener() {
+    btnLogin=(Button) findViewById(R.id.loginBut);
+    btnForgot=(Button) findViewById(R.id.forgotBut);
+    txtUser=(TextView) findViewById(R.id.usernameText);
+    txtPass=(TextView) findViewById(R.id.passwordText);
+    btnLogin.setOnClickListener(new View.OnClickListener() {
         @Override
         public void onClick(View v) {
-            Intent intent=new Intent(LoginScreenActivity.this,HomeScreenActivity.class);
-            startActivity(intent);
+            if (txtUser.getText().length()!=0 && txtPass.getText().length()!=0){
+                Toast.makeText(LoginScreenActivity.this,"Đăng nhập thành công",Toast.LENGTH_SHORT).show();
+                Intent intent=new Intent(LoginScreenActivity.this,HomeScreenActivity.class);
+                startActivity(intent);
+            }
+            else {
+                Toast.makeText(LoginScreenActivity.this,"Mời bạn nhập đủ thông tin",Toast.LENGTH_SHORT).show();
+            }
         }
     });
-    txtNd=(TextView) findViewById(R.id.usernameText);
-    txtNd.setText("");
-    txtNd=(TextView) findViewById(R.id.passwordText);
-    txtNd.setText("");
+
     }
 
     // test loginscreen
