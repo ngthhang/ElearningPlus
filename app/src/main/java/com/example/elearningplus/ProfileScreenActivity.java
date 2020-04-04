2fc9a74c0e271163b1366efa0b7c5643ea26a92e
sieutini4444 <sieutini4444@gmail.com>
2020-04-04 21:41:04 +0700

    FULL demo (nảy quên Stage all)

--- app/src/main/java/com/example/elearningplus/ProfileScreenActivity.java
+++ app/src/main/java/com/example/elearningplus/ProfileScreenActivity.java
@@ -3,43 +3,48 @@ package com.example.elearningplus;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
-
-import android.annotation.SuppressLint;
 import android.content.Intent;
 import android.graphics.Bitmap;
-import android.graphics.BitmapFactory;
 import android.net.Uri;
 import android.os.Bundle;
 import android.provider.MediaStore;
-import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageButton;
-import android.widget.ImageView;
 import android.widget.ListView;
 import android.widget.PopupMenu;
-
+import android.widget.TextView;
 import com.google.android.material.bottomnavigation.BottomNavigationView;
-
+import com.google.firebase.database.ChildEventListener;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
-
 import de.hdodenhof.circleimageview.CircleImageView;
-
 
 public class ProfileScreenActivity extends AppCompatActivity {
     private static final int REQUEST_ID_IMAGE_CAPTURE = 100;
     private static final int REQUEST_ID_IMAGE_CHOOSE = 161;
 
+    public float k=0;
+    public int i=0;
+    public float m=0;
+
+    DatabaseReference mData;
     CircleImageView imgvAvatar;
     ListView listView;
     List<Profile_DiemSV> mlist;
     ImageButton imageButton;
     Button button;
+    TextView tvMSSV;
+    TextView tvTenSV;
+    TextView tvDTBs;
 
-    @SuppressLint("CutPasteId")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -71,22 +76,79 @@ public class ProfileScreenActivity extends AppCompatActivity {
                 return false;
             }
         });
+
         /*FINISH - HANDLE BOTTOM NAVIGATION*/
 
-        //Add database DiemSV
-        mlist = new ArrayList<>();
-
-        mlist.add(new Profile_DiemSV("CTRR",(float) 7.5));
-        mlist.add(new Profile_DiemSV("MMT",(float) 8));
-        mlist.add(new Profile_DiemSV("NMHĐH",(float) 6));
-        mlist.add(new Profile_DiemSV("SXTK",(float) 8.2));
-        mlist.add(new Profile_DiemSV("PTƯDDĐ",(float) 9.3));
-
-        //Truyền dữ liệu vào adapter
+        //Ánh xạ
+        tvTenSV = findViewById(R.id.tvTenSV);
+        tvMSSV =findViewById(R.id.tvMSSV);
+        tvDTBs = findViewById(R.id.tvDTBs);
         listView=findViewById(R.id.listDiemSV);
-        Profile_DiemSV_Adapter adapter=new Profile_DiemSV_Adapter(this,mlist);
+
+        //Khởi tạo mData
+        mData = FirebaseDatabase.getInstance().getReference();
+
+        //Use database Ten
+        mData.child("student").child("-namez").addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                tvTenSV.setText(dataSnapshot.getValue().toString());
+            }
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+            }
+        });
+        //Use database MSSV
+        mData.child("student").child("id").addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                tvMSSV.setText(dataSnapshot.getValue().toString());
+            }
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+            }
+        });
+
+        //Add database
+        //mData.child("monhoc").push().setValue(new Profile_DiemSV("NMHĐH",(float) 6.5));
+
+        //Use database DiemSV
+        mlist = new ArrayList<>();
+        final Profile_DiemSV_Adapter adapter=new Profile_DiemSV_Adapter(this,mlist);
         listView.setAdapter(adapter);
-        adapter.notifyDataSetChanged();
+
+        mData.child("monhoc").addChildEventListener(new ChildEventListener() {
+            @Override
+            public void onChildAdded(@NonNull DataSnapshot dataSnapshot, @Nullable String s) {
+                Profile_DiemSV diemSV = dataSnapshot.getValue(Profile_DiemSV.class);
+                mlist.add(new Profile_DiemSV(diemSV.tenMH,diemSV.diemMH));
+                i+=1;
+                k=k+diemSV.diemMH;
+                m=k/i;
+                tvDTBs.setText(String.format("%.3g%n",m));
+                adapter.notifyDataSetChanged();
+            }
+
+            @Override
+            public void onChildChanged(@NonNull DataSnapshot dataSnapshot, @Nullable String s) {
+
+            }
+
+            @Override
+            public void onChildRemoved(@NonNull DataSnapshot dataSnapshot) {
+
+            }
+
+            @Override
+            public void onChildMoved(@NonNull DataSnapshot dataSnapshot, @Nullable String s) {
+
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+
+            }
+        });
 
         //Thiết lập sự kiện button log out
         button = findViewById(R.id.btnLogOut);
@@ -98,8 +160,7 @@ public class ProfileScreenActivity extends AppCompatActivity {
             }
         });
 
-
-        //Thiết lập sự kiện imagebutton Camera
+        //button, imagebutton Camera
         imgvAvatar = findViewById(R.id.imgAvatar);
         imageButton = findViewById(R.id.imgbCamera);
         imageButton.setOnClickListener(new View.OnClickListener() {
@@ -110,6 +171,7 @@ public class ProfileScreenActivity extends AppCompatActivity {
         });
     }
 
+    //Menu for Camera
     private void ShowMenu(){
         final PopupMenu popupMenu = new PopupMenu(this,imageButton);
         popupMenu.getMenuInflater().inflate(R.menu.menu_profile,popupMenu.getMenu());
@@ -132,16 +194,18 @@ public class ProfileScreenActivity extends AppCompatActivity {
         });
     }
 
+    //Make a photo
     @Override
     protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
         super.onActivityResult(requestCode, resultCode, data);
+        //Capture
         if (requestCode==REQUEST_ID_IMAGE_CAPTURE && resultCode== RESULT_OK && data!=null) {
             Bitmap bitmap1 = (Bitmap) data.getExtras().get("data");
             imgvAvatar.setImageBitmap(bitmap1);
         }
         else if(requestCode == REQUEST_ID_IMAGE_CHOOSE && resultCode == RESULT_OK) {
             try {
-                //xử lý lấy ảnh chọn từ điện thoại:
+                //Choose
                 Uri imageUri = data.getData();
                 Bitmap bitmap2 = MediaStore.Images.Media.getBitmap(this.getContentResolver(), imageUri);
                 imgvAvatar.setImageBitmap(bitmap2);
@@ -149,11 +213,5 @@ public class ProfileScreenActivity extends AppCompatActivity {
                 e.printStackTrace();
             }
         }
-    }
-
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        getMenuInflater().inflate(R.menu.menu_profile,menu);
-        return super.onCreateOptionsMenu(menu);
     }
 }
