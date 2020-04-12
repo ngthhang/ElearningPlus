d3ec3f7b4adb3d1bdb3771af4e64d1e53660c6f4
sieutini4444 <sieutini4444@gmail.com>
2020-04-12 14:18:44 +0700

    add AssignmentView + firebase + menuback

--- app/src/main/java/com/example/elearningplus/AssignmentViewScreen.java
+++ app/src/main/java/com/example/elearningplus/AssignmentViewScreen.java
@@ -5,18 +5,43 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
+import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ListView;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import com.google.android.material.bottomnavigation.BottomNavigationView;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
 public class AssignmentViewScreen extends AppCompatActivity {
+    DatabaseReference courseDtb;
+    DatabaseReference mCuorse;
+    TextView tvLab, tvDue, tvContent;
+    EditText edtUrl;
+    Button btnSubmit;
+    String m;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.assignment_view);
 
-
+        courseDtb = FirebaseDatabase.getInstance().getReference();
+        tvLab = findViewById(R.id.tvLab);
+        tvContent = findViewById(R.id.tvContent);
+        tvDue = findViewById(R.id.tvDue);
+        edtUrl = findViewById(R.id.edtUrl);
+        btnSubmit = findViewById(R.id.btnSubmit);
 
 
 //        /*START - HANDLE BOTTOM NAVIGATION */
@@ -47,5 +72,81 @@ public class AssignmentViewScreen extends AppCompatActivity {
 //        });
 //        /*FINISH - HANDLE BOTTOM NAVIGATION*/
 
+        mCuorse = courseDtb.child("course").child("CSDL").child("assignment").child("0");
+        mCuorse.child("name").addListenerForSingleValueEvent(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                String name = (String) dataSnapshot.getValue();
+                tvLab.setText(name);
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+            }
+        });
+
+        mCuorse.child("due").addListenerForSingleValueEvent(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                String due = (String) dataSnapshot.getValue();
+                due = due + "\nPoint: 0\nSubmitting: a file upload";
+                tvDue.setText(due);
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+            }
+        });
+
+        mCuorse.child("content").addListenerForSingleValueEvent(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                String content = (String) dataSnapshot.getValue();
+                tvContent.setText(content);
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+            }
+        });
+
+        edtUrl.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+
+            }
+
+            @Override
+            public void afterTextChanged(Editable s) {
+                m = String.valueOf(s);
+            }
+        });
+
+        btnSubmit.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(AssignmentViewScreen.this, m, Toast.LENGTH_SHORT).show();
+            }
+        });
+
     }
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_backk,menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        if (item.getItemId() == R.id.menuBackk){
+            Toast.makeText(this, "Finish", Toast.LENGTH_SHORT).show();
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
 }
