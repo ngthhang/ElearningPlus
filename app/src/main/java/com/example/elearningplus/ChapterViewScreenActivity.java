cb71fa9e72ffcc6f9e162c31e409ad594ae14441
sieutini4444 <sieutini4444@gmail.com>
2020-04-20 13:07:31 +0700

    add leson layout

--- app/src/main/java/com/example/elearningplus/ChapterViewScreenActivity.java
+++ app/src/main/java/com/example/elearningplus/ChapterViewScreenActivity.java
@@ -3,6 +3,9 @@ package com.example.elearningplus;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.MenuItem;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
@@ -10,12 +13,22 @@ import androidx.appcompat.app.AppCompatActivity;
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 public class ChapterViewScreenActivity extends AppCompatActivity {
+    Button btnBackward, btnForward;
+    TextView tvChapternumber, tvChaptercontent, tvChapterdetail1, tvChapterdetail2;
+    ImageView imgvAnh;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_chapter_view_screen);
 
+        btnBackward = findViewById(R.id.btnBackward);
+        btnForward = findViewById(R.id.btnForward);
+        tvChapternumber = findViewById(R.id.tvChapternumber);
+        tvChaptercontent = findViewById(R.id.tvChaptercontent);
+        tvChapterdetail1 = findViewById(R.id.tvChapterdetail1);
+        tvChapterdetail2 = findViewById(R.id.tvChapterdetail2);
+        imgvAnh = findViewById(R.id.imgvAnh);
 
         /*START - HANDLE BOTTOM NAVIGATION */
         //Initial and assign variable
